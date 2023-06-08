74aa57c2c2dbc970877191ea69c134e24fe393aa
Subinay Adhikary <50978159+subinayadhikary@users.noreply.github.com>
2023-06-08 13:37:58 +0530

    Update and rename top_tf_idf_v3.py to Tf_idf.py

--- Tf_idf.py
+++ Tf_idf.py
@@ -11,51 +11,34 @@ import nltk
 import json
 stopwords=[]
 ps = PorterStemmer()
+vector={}
 from nltk.stem.wordnet import WordNetLemmatizer
-json_embedding_tf_idf=open("/home/subinay/Documents/data/pairwise_similarity/tf_idf/idf_value_corpus.json")
-embedding= json.load(json_embedding_tf_idf)
-dir_annotated="/home/subinay/Documents/data/pairwise_similarity/processed_200_docs/"
-def top_terms(score,term):
-    #print(score)
-    #print(len(term))
+json_value_idf=open("/home/subinay/Documents/data/pairwise_similarity/tf_idf/idf_value_corpus.json")
+Idf_value= json.load(json_value_idf)
+dir_annotated="/home/subinay/Documents/data/pairwise_similarity/Second_50/50_docs/"
+def top_terms(score,term):  # to find embedding of top tf-idf terms
     feature_scores = {}
     for i in range(len(score)):
-        #print(term[i],"##",score[i])
-        feature_scores[term[i]]=score[i]*embedding[term[i]]
+        feature_scores[term[i]]=score[i]*Idf_value[term[i]]
     term_score=dict(sorted(feature_scores.items(), key=lambda item: item[1],reverse=True))
     docs1=[]
-    
     for key,value in term_score.items():
-            #docs1.append(key)
         if(value!=0):
             docs1.append(key)
-        
     num_to_select = int(0.1* len(docs1)) # to select top terms based on their tf-idf value
-    # select the first 20% of the elements
-    #print(num_to_select)
+    # select the first 10% of the elements
     selected_elems = docs1[:num_to_select]
-    tfidf_value=[]
-    for i in range(len(score)):
-        tfidf_value.insert(i,0)
-    term=list(term)
-    #print(selected_elems)
+    tfidf_value={}
     for elem in selected_elems:
-        index=term.index(elem)
-        value=feature_scores[elem]
-        tfidf_value[index]=value
-    #print(len(tfidf_value))
-
-    #print(len(selected_elems))
-    # print the selected elements
-    #text=" ".join(selected_elems)
+        tfidf_value[elem]=feature_scores[elem]
     return tfidf_value          
 docs=[]
 docid=[]
 files=os.listdir(dir_annotated)
-for file in files:
+for file in files: 
     docid.append(file)
     text=""
-    with open("/home/subinay/Documents/data/pairwise_similarity/processed_200_docs/"+file,"r") as f1:
+    with open("/home/subinay/Documents/data/pairwise_similarity/Second_50/50_docs/"+file,"r") as f1:
         for line in f1:
             line=line.strip()
             text=text+line
@@ -70,63 +53,24 @@ stemmer = PorterStemmer()
 analyzer = vectorizer.build_analyzer()
 vectorizer.set_params(tokenizer=lambda doc: (stemmer.stem(token) for token in analyzer(doc)))
 X = vectorizer.fit_transform(docs)
-
-#vectorizer = CountVectorizer(stop_words=custom_stopwords)
-#vectorizer = CountVectorizer(analyzer=stemmed_words)
-
-# Fit the vectorizer and transform the text data
-#X = vectorizer.fit_transform(corpus)
-
 # Access the vocabulary and feature names
 vocabulary = vectorizer.vocabulary_
 term= vectorizer.get_feature_names()
-
-# Print the matrix of token counts
-#print(X.toarray())
-
-# Print the vocabulary
-#print(vocabulary)
-
-# Print the feature names
-#print(feature_names)
 X1=[]
 score=[]
 count=0
-for i in range(0,200):
+for i in range(0,50):
     count=count+1
     x=X[i,].toarray()
-    # x2=X[i,].toarray().tolist()
-    # score2.append(x2)
     x1=x.flatten()
-    #print(len(x1))
     score.append(x1)
 for i in range(len(score)):
-    print(docid[i])
     text=top_terms(score[i],term)
     X1.append(text)
-vector={}
 for i in range(len(docid)):
     vector[docid[i]]=X1[i]
-print(len(vector))
-with open("tf_idf_embedding_200docs.json", "w") as outfile:
+with open("tf_idf_embedding_50docs.json", "w") as outfile:
      json.dump(vector, outfile)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
